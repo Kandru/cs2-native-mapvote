064b413b05477bf6bee336e3df74bdf5c947c522
Kalle <kalle@kandru.de>
2025-03-25 16:49:56 +0100

    bump version to 0.0.3

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace NativeMapVote
 {
     public partial class NativeMapVote
     {
-        public override string ModuleVersion => "0.0.2";
+        public override string ModuleVersion => "0.0.3";
     }
 }
